e4831aea615e95cd01625ae022d3c04a8a1c10b0
Umer Khalil <umerkhalil515k@gmail.com>
2025-09-20 23:11:16 +0500

    test change for CI/CD3

--- application.py
+++ application.py
@@ -2,25 +2,31 @@ import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
-application = Flask(__name__) #Initialize the flask App
+application = Flask(__name__)  # Initialize the flask App
 model = pickle.load(open('model.pkl', 'rb'))
+
 
 @application.route('/')
 def home():
     return render_template('index.html')
 
-@application.route('/predict',methods=['POST'])
+
+@application.route('/predict', methods=['POST'])
 def predict():
-    '''
+    """
     For rendering results on HTML GUI
-    '''
+    """
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template(
+        'index.html',
+        prediction_text=f'Employee Salary should be $ {output}'
+    )
+
 
 if __name__ == "__main__":
     application.run(debug=True)
--- tests/test_model.py
+++ tests/test_model.py
@@ -4,11 +4,13 @@ import numpy as np
 # Load the model from pickle
 model = pickle.load(open("model.pkl", "rb"))
 
+
 def test_prediction_shape():
     """Check if model returns prediction with correct shape"""
     features = np.array([[2, 9, 6]])
     prediction = model.predict(features)
     assert prediction.shape == (1,)
+
 
 def test_prediction_type():
     """Check if prediction is a float or int"""
